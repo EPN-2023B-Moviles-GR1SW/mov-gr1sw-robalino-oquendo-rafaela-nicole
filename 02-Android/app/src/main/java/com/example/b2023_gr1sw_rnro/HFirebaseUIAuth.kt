5eb2fadd6bd9945c0bb594bc2b8127a0db6e3dc5
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2024-02-06 21:10:39 -0500

    clase 31/01

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnro/HFirebaseUIAuth.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnro/HFirebaseUIAuth.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
@@ -49,5 +50,46 @@ class HFirebaseUIAuth : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hfirebase_uiauth)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        btnLogin.setOnClickListener {
+            val providers = arrayListOf(
+                // Arreglo de PROVIDERS para logearse
+                // EJ: Correo, Facebook, Twitter, Google,
+                AuthUI.IdpConfig.EmailBuilder().build()
+            )
+            // Construimos el intent de login
+            val logearseIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+            // RESPUESTA DEL INTENT DE LOGIN
+            respuestaLoginAuthUi.launch(logearseIntent)
+            // https://console.firebase.google.com/u/0/project/PROYECTO/authentication/settings
+            // Authentication/Settings/UserActions/Email enumeration protection ( )
+            // Unchecked!!
+        }
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        btnLogout.setOnClickListener { seDeslogeo() }
+        // Logica si destruye el aplicativo
+        val usuario = FirebaseAuth.getInstance().currentUser
+        if(usuario != null){
+            val tvBienvenido = findViewById<TextView>(R.id.tv_bienvenido)
+            val btnLogin: Button = findViewById<Button>(R.id.btn_login_firebase)
+            val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+            btnLogout.visibility = View.VISIBLE
+            btnLogin.visibility = View.INVISIBLE
+            tvBienvenido.text = usuario.displayName
+        }
+    }
+
+    fun seDeslogeo(){
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        val tvBienvenido = findViewById<TextView>(R.id.tv_bienvenido)
+        tvBienvenido.text = "Bienvenido"
+        btnLogout.visibility = View.INVISIBLE
+        btnLogin.visibility = View.VISIBLE
+        FirebaseAuth.getInstance().signOut()
     }
 }
